1c4c4af52abcef5715c2cf2cf339e625a036038d
Jørgen Jacobsen <jorgeja@gmail.com>
2015-12-23 19:52:02 +0100

    updated game

--- game.js
+++ game.js
@@ -1,54 +1,288 @@
 var gameSettings = {
 	lives: 3,
 	name: "funkyface",
-	level: 2,
+	currentlevel: 0,
+	currentmap: 0,
 	width: window.innerWidth,
 	height: window.innerHeight,
+	levelHeight: window.innerHeight/2,
 	standardLevelScreen : $("#standardLevel"),
 	mainScreen: $("mainscreen")
 }
 
 
+var translation = {
+	"startLevel": "Start level",
+	"newgame": "New game"
+}
 
-var questions = [
+var maps = [
 	{
-		question: "Who created sonic?",
-		alternatives: ["Nintendo","Sega","Sony Ericsson"],
-		answer: 1
+		"mapname": "Music world",
+		"levels": [
+			{
+				"levelname": "The amazon",
+				"leveltype": "standardlevel",
+				"correctAnswers": 5,
+				"questions": 
+				[
+					{
+						question: "Who created sonic?",
+						alternatives: ["Nintendo","Sega","Sony Ericsson"],
+						answer: 1
+					},
+					{
+						question: "Who created Mario?",
+						alternatives: ["Sega","Atari","Nintendo"],
+						answer: 2
+					},
+					{
+						question: "Who created Pitfall?",
+						alternatives: ["Sega","Atari","Nintendo"],
+						answer: 1
+					},
+					{
+						question: "Where does Nintendo come from?",
+						alternatives: ["China","Japan","Korea"],
+						answer: 1
+					},
+					{
+						question: "Who created Mario?",
+						alternatives: ["Satoru Iwata","Shigeru Miyamoto","Bill Gates"],
+						answer: 1
+					}
+				],
+				"timeToBeatLevel": 30
+			},
+			{
+				"levelname": "Battle creek",
+				"leveltype": "standardlevel",
+				"correctAnswers": 3,
+				"questions": 
+				[
+					{
+						question: "Who created sonic?",
+						alternatives: ["Nintendo","Sega","Sony Ericsson"],
+						answer: 1
+					},
+					{
+						question: "Who created Mario?",
+						alternatives: ["Sega","Atari","Nintendo"],
+						answer: 2
+					},
+					{
+						question: "Who created Pitfall?",
+						alternatives: ["Sega","Atari","Nintendo"],
+						answer: 1
+					},
+					{
+						question: "Where does Nintendo come from?",
+						alternatives: ["China","Japan","Korea"],
+						answer: 1
+					},
+					{
+						question: "Who created Mario?",
+						alternatives: ["Satoru Iwata","Shigeru Miyamoto","Bill Gates"],
+						answer: 1
+					}
+				],
+				"timeToBeatLevel": 30
+			},
+			{
+				"levelname": "The crazy crack",
+				"leveltype": "standardlevel",
+				"correctAnswers": 1,
+				"questions": 
+				[
+					{
+						question: "Who created sonic?",
+						alternatives: ["Nintendo","Sega","Sony Ericsson"],
+						answer: 1
+					},
+					{
+						question: "Who created Mario?",
+						alternatives: ["Sega","Atari","Nintendo"],
+						answer: 2
+					},
+					{
+						question: "Who created Pitfall?",
+						alternatives: ["Sega","Atari","Nintendo"],
+						answer: 1
+					},
+					{
+						question: "Where does Nintendo come from?",
+						alternatives: ["China","Japan","Korea"],
+						answer: 1
+					},
+					{
+						question: "Who created Mario?",
+						alternatives: ["Satoru Iwata","Shigeru Miyamoto","Bill Gates"],
+						answer: 1
+					}
+				],
+				"timeToBeatLevel": 10
+			},
+			{
+				"levelname": "Cookie cutter",
+				"leveltype": "standardlevel",
+				"correctAnswers": 1,
+				"questions": 
+				[
+					{
+						question: "Who created sonic?",
+						alternatives: ["Nintendo","Sega","Sony Ericsson"],
+						answer: 1
+					},
+					{
+						question: "Who created Mario?",
+						alternatives: ["Sega","Atari","Nintendo"],
+						answer: 2
+					},
+					{
+						question: "Who created Pitfall?",
+						alternatives: ["Sega","Atari","Nintendo"],
+						answer: 1
+					},
+					{
+						question: "Where does Nintendo come from?",
+						alternatives: ["China","Japan","Korea"],
+						answer: 1
+					},
+					{
+						question: "Who created Mario?",
+						alternatives: ["Satoru Iwata","Shigeru Miyamoto","Bill Gates"],
+						answer: 1
+					}
+				],
+				"timeToBeatLevel": 10
+			},
+			{
+				"levelname": "Crappy cutter",
+				"leveltype": "standardlevel",
+				"correctAnswers": 1,
+				"questions": 
+				[
+					{
+						question: "Who created sonic?",
+						alternatives: ["Nintendo","Sega","Sony Ericsson"],
+						answer: 1
+					},
+					{
+						question: "Who created Mario?",
+						alternatives: ["Sega","Atari","Nintendo"],
+						answer: 2
+					},
+					{
+						question: "Who created Pitfall?",
+						alternatives: ["Sega","Atari","Nintendo"],
+						answer: 1
+					},
+					{
+						question: "Where does Nintendo come from?",
+						alternatives: ["China","Japan","Korea"],
+						answer: 1
+					},
+					{
+						question: "Who created Mario?",
+						alternatives: ["Satoru Iwata","Shigeru Miyamoto","Bill Gates"],
+						answer: 1
+					}
+				],
+				"timeToBeatLevel": 10
+			}
+		]
 	},
-	{
-		question: "Who created Mario?",
-		alternatives: ["Sega","Atari","Nintendo"],
-		answer: 2
-	},
-	{
-		question: "Who created Pitfall?",
-		alternatives: ["Sega","Atari","Nintendo"],
-		answer: 1
-	}
+
 ]
+
+
 console.log($("#start-game"));
 $("#start-game").on("click", function () {
-	startLevel();
+	openMap();
 });
 
-var startLevel = function () {
+
+
+var startLevel = function (mapId, levelId) {
 	$("#mainscreen").hide();
+	$("#mapscreen").hide();
 	$("#standardlevel").show();
-	console.log("hest");
-	StandardLevel(questions, 30, 3, gameSettings, 
-		function () {
-			console.log("success");
-		},
-		function () {
-			console.log('fail');
-		}
-	);
+	console.log(mapId, levelId);
+	var levelStart = maps[mapId];
+	console.log(levelStart);
+	if(levelStart.leveltype == "standardlevel") {
+		StandardLevel(levelStart.questions, levelStart.timeToBeatLevel, levelStart.correctAnswers, gameSettings, 
+			function () {
+				console.log("success");
+			},
+			function () {
+				console.log('fail');
+			}
+		);	
+	}
+	
+}
+
+
+var renderMap = function (maps) {
+	var mapContainer = $("#mapscreen");
+
+	var showLevel = function (event) {
+		var levelMarker = $(event.target);
+		levelMarker.parent().siblings().removeClass("selected");
+		levelMarker.parent().addClass("selected");
+
+	};
+
+	var enterLevel = function (event) {
+		console.log('heeey');
+		var levelStartButton = $(event.target);
+		console.log(levelStartButton, levelStartButton.parent());
+		var levelId = levelStartButton.parent().attr("data-level-id");
+		var mapId = levelStartButton.parent().attr("data-map-id");
+		startLevel(mapId, levelId);
+	};
+	maps.forEach(function(map, mapIndex) {
+		var mapScreenContainer = $("<div></div>").addClass("map-screen-container");
+		var mapTitle = $("<div></div>").addClass("map-title").text(map.mapname);
+		var mapBuffer = $("<div></div>").addClass("map-buffer");
+		mapScreenContainer.append(mapTitle, mapBuffer);
+
+		TweenMax.to(mapScreenContainer, 0, {x: (mapIndex-gameSettings.currentmap)*gameSettings.width, ease: Linear.easeNone});
+
+		map.levels.forEach(function (level, levelIndex) {
+			var levelContainer = $("<div></div>").addClass("level").css({
+				'height': gameSettings.levelHeight
+			})
+			.attr("data-map-id", mapIndex)
+			.attr("data-level-id", levelIndex);
+			var levelTitle = $("<div></div>").addClass("level-title").text(level.levelname);
+			var levelMarker = $("<div></div>").addClass("level-marker");
+			var levelStartButton = $("<div></div>").addClass("level-start-button").text(translation.startLevel);
+			if(mapIndex == gameSettings.currentmap && levelIndex == gameSettings.currentlevel) {
+				levelContainer.addClass("current").addClass("selected");
+			}
+			levelContainer.append(levelMarker, levelTitle, levelStartButton);
+			mapScreenContainer.prepend(levelContainer);
+			levelMarker.on('click', showLevel);
+			levelStartButton.on('click', enterLevel);
+		});
+
+		mapScreenContainer.scrollTop(mapScreenContainer[0].scrollHeight);
+		mapContainer.append(mapScreenContainer);
+	});
+};
+
+var openMap = function (settings) {
+	$("#mainscreen").hide();
+	$("#standardlevel").hide();
+	$("#mapscreen").show();
 }
 
 
 var StandardLevel = function (questions, time, numberOfQuestions, settings, success, fail) {
 	var standardLevelContainer = $("#standardlevel");
+	standardLevelContainer.empty();
 
 	var questionCounter = 1;
 
@@ -111,3 +345,9 @@ var StandardLevel = function (questions, time, numberOfQuestions, settings, succ
 	startTimer();
 	generateQuestion(questions[questionCounter-1]);
 }
+
+
+
+renderMap(maps, function () {
+	console.log("start spill");
+});
