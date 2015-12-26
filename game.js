591f454a0070e0080074fd62c098b6b48f87f379
Jørgen Jacobsen <retrojorgen@Jrgens-MacBook-Air.local>
2015-12-26 21:55:02 +0100

    added gap

--- game.js
+++ game.js
@@ -116,6 +116,102 @@ var openMap = function (settings) {
 }
 
 
+
+var gapLevel = function (questions, time, timeBetweenGaps, settings, success, fail, achievements) {
+	var gapLevelContainer = $("#gaplevel");
+	var levelLives = settings.lives;
+	gapLevelContainer.empty();
+
+
+	var clearGap = false;
+
+	var questionCounter = 1;
+
+	var levelTimer = setTimeout(function () {
+		failLevel();
+	}, time * 1000);
+
+	var questionTimeout = undefined;
+
+
+	var generateQuestion = function (questionObject, newTimer) {
+		var questionContainer = $("<div></div>").addClass("question-container").addClass("current");
+		var question = $("<div></div>").addClass("question").text(questionObject.question);
+		questionContainer.append(question);
+		questionObject.alternatives.forEach(function (alternative, index) {
+			var answer = $("<div></div>").addClass("answer").text(alternative);
+			var answerTouch = new Hammer(answer[0]);
+			if(index == questionObject.answer) {
+				answer.addClass("correct");
+			}
+			answerTouch.on("tap", checkAnswer);
+			questionContainer.append(answer);
+		});
+
+		TweenMax.to(questionContainer, 0, {x: window.innerWidth, ease:Linear.easeNone});
+		gapLevelContainer.append(questionContainer);
+		TweenMax.to(questionContainer, 0.5, {x: 0, ease: Back.easeOut});
+
+		if(newTimer) {
+			clearGap = false;
+			questionTimeout = setTimeout(function () {
+				console.log(questionTimeout);
+				checkGap(questionContainer);
+			}, timeBetweenGaps * 100);
+		}
+
+		console.log(questionTimeout);
+	};
+
+	var checkGap = function (questionContainer) {
+		if(!clearGap) {
+			clearTimeout(questionTimeout);
+			generateNewQuestion(parentContainer, true);
+		} else {
+			clearTimeout(questionTimeout);
+			failLevel();
+		}
+	}
+
+	var checkAnswer = function (event) {
+		var currentAnswer = $(event.target);
+		var parentContainer = currentAnswer.parent();
+		if(currentAnswer.hasClass("correct")) {
+			currentAnswer.addClass("correct-animation");
+			clearGap = true;
+		} else {
+			currentAnswer.addClass("wrong-animation");
+			clearGap = false;
+		}
+	};
+
+	var generateNewQuestion = function (questionContainer, toggle) {
+		TweenMax.to(questionContainer, 0.5, {x: -window.innerWidth, ease: Back.easeOut});
+		generateQuestion(questions[questionCounter++], toggle);
+	};
+
+	var startTimer = function () {
+		var timerContainer = $("<div></div>").addClass("level-timer");
+		TweenMax.to(timerContainer, 0, {x: -window.innerWidth, ease: Linear.easeNone});
+		TweenMax.to(timerContainer, time, {x: 0, ease: Linear.easeNone});
+		standardLevelContainer.append(timerContainer);
+	}
+
+	var failLevel = function () {
+		clearTimeout(levelTimer);
+		fail();
+	}
+
+	var winLevel = function () {
+		clearTimeout(levelTimer);
+		success();
+	}
+
+	startTimer();
+	generateQuestion(questions[questionCounter-1], true);
+}
+
+
 var hordeLevel = function (questions, time, numberOfQuestions, settings, success, fail) {
 	var standardLevelContainer = $("#standardlevel");
 	var levelLives = settings.lives;
@@ -172,7 +268,7 @@ var hordeLevel = function (questions, time, numberOfQuestions, settings, success
 
 	var successAttack = function (questionContainer) {
 		clearTimeout(questionTimeout);
-		generateNewQuestion(parentContainer, true);
+		generateNewQuestion(questionContainer, true);
 	};
 
 	var checkAnswer = function (event) {
